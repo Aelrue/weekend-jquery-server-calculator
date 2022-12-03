14a0fd69cf6288562c420bf4bfab1d6f20830ead
Aelrue <101987454+Aelrue@users.noreply.github.com>
2022-12-03 12:07:54 -0600

    removed button types, added click events for all buttons, put firstValue and second in postEquation

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,22 +1,55 @@
-let equations = [];
-
 $(document).ready(onReady);
 
 function onReady() {
   console.log("DOM loaded");
-  $("submit").on("click");
+  $("#plus").on("click", appendToDom);
+  $("#minus").on("click", appendToDom);
+  $("#multiply").on("click", appendToDom);
+  $("#divide").on("click", appendToDom);
+  $("#submit").on("click", appendToDom);
+  $("#reset").on("click", appendToDom);
 }
 
-function addEquation() {
-  console.log("in addEquation");
-  let equation = {
-    firstValue: $("#firstValueIn").val(),
-    secondValue: $("#secondValueIn").val(),
-  };
-  equations.push(equation);
-  $("#firstValueIn").val("");
-  $("#secondValueIn").val("");
-  appendToDom();
+let equations = {
+  firstValue: $("#firstValueIn").val(),
+  secondValue: $("#secondValueIn").val(),
+};
+
+// function addEquation() {
+//   console.log("in addEquation");
+//   let equation = {
+//     firstValue: $("#firstValueIn").val(),
+//     secondValue: $("#secondValueIn").val(),
+//   };
+//   equations.push(equation);
+//   $("#firstValueIn").val("");
+//   $("#secondValueIn").val("");
+// }
+
+function postEquation() {
+  $.ajax({
+    method: "POST",
+    url: "/equations",
+    data: equations,
+  })
+    .then(function (response) {
+      console.log("post response", response);
+      getEquations();
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert(error.responseText);
+    });
+}
+
+function getEquation() {
+  $.ajax({
+    method: "GET",
+    url: "/equations",
+  }).then(function (response) {
+    console.log("response from server", response);
+    appendToDom(response);
+  });
 }
 
 function appendToDom(array) {
@@ -24,7 +57,7 @@ function appendToDom(array) {
   $("#output").empty();
   for (let object of array) {
     $("#output").append(`
-    <li>"
+    <li>${object.firstValue}${object.secondValue}</li>
     `);
   }
 }
